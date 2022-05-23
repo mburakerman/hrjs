9772bf8c6d22bf078ca5144554f5c4e8611274eb
mburakerman <mburakerman@gmail.com>
2022-05-23 13:48:09 +0300

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    amd: true,
+    jest: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+  }
+}
--- src/hr.test.js
+++ src/hr.test.js
@@ -1,45 +1,45 @@
-const HR = require('./hr.js');
+const HR = require('./hr.js')
 
 describe('HR.js', () => {
   test('highlight backgroundColor should match', async () => {
-    const defaultText = 'lorem ipsm dolor sit amet.';
-    document.body.innerHTML = `<p id="test-1">${defaultText}</p>`;
+    const defaultText = 'lorem ipsm dolor sit amet.'
+    document.body.innerHTML = `<p id="test-1">${defaultText}</p>`
 
     new HR('#test-1', {
-      highlight: defaultText,
-    }).hr();
+      highlight: defaultText
+    }).hr()
 
-    const testElement = document.getElementById('test-1');
-    expect(testElement.children[0].style.backgroundColor).toBe('rgb(255, 222, 112)');
-  });
+    const testElement = document.getElementById('test-1')
+    expect(testElement.children[0].style.backgroundColor).toBe('rgb(255, 222, 112)')
+  })
 
   test('replaceWith & backgroundColor values should match', async () => {
-    const defaultText = 'black';
-    const replacedText = 'white';
-    document.body.innerHTML = `<p id="test-2">${defaultText}</p>`;
+    const defaultText = 'black'
+    const replacedText = 'white'
+    document.body.innerHTML = `<p id="test-2">${defaultText}</p>`
 
     new HR('#test-2', {
       highlight: defaultText,
       replaceWith: replacedText,
-      backgroundColor: 'rgb(180, 255, 235)',
-    }).hr();
+      backgroundColor: 'rgb(180, 255, 235)'
+    }).hr()
 
-    const testElement = document.getElementById('test-2');
-    expect(testElement.textContent).toBe(replacedText);
-    expect(testElement.children[0].style.backgroundColor).toBe('rgb(180, 255, 235)');
-  });
+    const testElement = document.getElementById('test-2')
+    expect(testElement.textContent).toBe(replacedText)
+    expect(testElement.children[0].style.backgroundColor).toBe('rgb(180, 255, 235)')
+  })
 
   test('array of items replace should match', async () => {
-    const defaultText = 'cat bird';
-    const replacedText = 'dog unicorn';
-    document.body.innerHTML = `<p id="test-3">${defaultText}</p>`;
+    const defaultText = 'cat bird'
+    const replacedText = 'dog unicorn'
+    document.body.innerHTML = `<p id="test-3">${defaultText}</p>`
 
     new HR('#test-3', {
-      highlight: defaultText.split(" "),
-      replaceWith: replacedText.split(" "),
-    }).hr();
+      highlight: defaultText.split(' '),
+      replaceWith: replacedText.split(' ')
+    }).hr()
 
-    const testElement = document.getElementById('test-3');
-    expect(testElement.textContent.trim()).toBe(replacedText);
-  });
-});
+    const testElement = document.getElementById('test-3')
+    expect(testElement.textContent.trim()).toBe(replacedText)
+  })
+})
